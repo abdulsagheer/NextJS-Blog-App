3dd2165a3febedf68b49ad548bb7fc9e120ecc9e
abdulsagheer <abdulsagheeras29@gmail.com>
2023-02-22 23:38:57 +0530

    API request to get server session

--- app/Nav.tsx
+++ app/Nav.tsx
@@ -1,21 +1,21 @@
-import { unstable_getServerSession } from "next-auth/next"
-import { authOptions } from "../pages/api/auth/[...nextauth]"
-import { signIn } from "next-auth/react"
-import Login from "./auth/Login"
-import Logged from "./auth/Logged"
-import Link from "next/link"
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../pages/api/auth/[...nextauth]";
+import { signIn } from "next-auth/react";
+import Login from "./auth/Login";
+import Logged from "./auth/Logged";
+import Link from "next/link";
 
 export default async function Nav() {
-  const session = await unstable_getServerSession(authOptions)
+	const session = await getServerSession(authOptions);
 
-  return (
-    <nav className="flex items-center justify-between p-8 ">
-      <Link href={"/"}>
-        <h1 className="text-lg font-bold">SendIt.</h1>
-      </Link>
-      <ul className="flex items-center gap-6"></ul>
-      {!session?.user && <Login />}
-      {session?.user && <Logged image={session.user.image || ""} />}
-    </nav>
-  )
+	return (
+		<nav className="flex items-center justify-between p-8 ">
+			<Link href={"/"}>
+				<h1 className="text-lg font-bold">Blog Post.</h1>
+			</Link>
+			<ul className="flex items-center gap-6"></ul>
+			{!session?.user && <Login />}
+			{session?.user && <Logged image={session.user.image || ""} />}
+		</nav>
+	);
 }
